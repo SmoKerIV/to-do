02370b3c762d217942382f32d5e1da2d7c1ebf0a
Baker Alazzawi <Baker.alazzawi0@gmail.com>
2023-10-23 19:07:34 +0300

    Final

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 const StartupIdeasApp = () => {
   const [ideas, setIdeas] = useState([]);
-
   const [newIdea, setNewIdea] = useState({
     title: "",
     description: "",
     priority: "Low",
   });
+  const [editingIdeaId, setEditingIdeaId] = useState(null);
+
+  useEffect(() => {
+    const storedIdeas = localStorage.getItem("startupIdeas");
+    if (storedIdeas) {
+      setIdeas(JSON.parse(storedIdeas));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("startupIdeas", JSON.stringify(ideas));
+  }, [ideas]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -23,11 +33,22 @@ const StartupIdeasApp = () => {
     setNewIdea({ title: "", description: "", priority: "Low" });
   };
 
-  const editIdea = (id, newIdeaData) => {
+  const startEdit = (id) => {
+    setEditingIdeaId(id);
+  };
+
+  const saveEdit = (id) => {
     const updatedIdeas = ideas.map((idea) =>
-      idea.id === id ? { ...idea, ...newIdeaData } : idea
+      idea.id === id
+        ? { ...idea, title: newIdea.title, description: newIdea.description }
+        : idea
     );
     setIdeas(updatedIdeas);
+    setEditingIdeaId(null);
+  };
+
+  const cancelEdit = () => {
+    setEditingIdeaId(null);
   };
 
   const deleteIdea = (id) => {
@@ -35,59 +56,43 @@ const StartupIdeasApp = () => {
     setIdeas(updatedIdeas);
   };
 
-  // Custom sorting function to sort by priority
   const sortedIdeas = [...ideas].sort((a, b) => {
     const priorityOrder = { High: 1, Medium: 2, Low: 3 };
     return priorityOrder[a.priority] - priorityOrder[b.priority];
   });
 
   return (
+    
     <div>
       <h1>Startup Ideas</h1>
-      <ul>
-        {sortedIdeas.map((idea) => (
-          <li key={idea.id}>
-            <div>
-              <strong>Title:</strong> {idea.title}
-            </div>
-            <div>
-              <strong>Description:</strong> {idea.description}
-            </div>
-            <div>
-              <strong>Priority:</strong> {idea.priority}
-            </div>
-            <button onClick={() => deleteIdea(idea.id)}>Delete</button>
-            <button onClick={() => editIdea(idea.id, { title: "New Title" })}>
-              Edit
-            </button>
-          </li>
-        ))}
-      </ul>
-
       <h2>Add New Idea</h2>
       <form>
-        <label>
-          Title:
+        <div className="input-container">
+          <label>
+            <strong>Title:</strong>
+          </label>
           <input
             type="text"
             name="title"
             value={newIdea.title}
             onChange={handleInputChange}
           />
-        </label>
-        <br />
-        <label>
-          Description:
+        </div>
+        <div className="input-container">
+          <label>
+            <strong>Description:</strong>
+          </label>
           <input
             type="text"
             name="description"
             value={newIdea.description}
             onChange={handleInputChange}
           />
-        </label>
-        <br />
-        <label>
-          Priority:
+        </div>
+        <div className="input-container">
+          <label>
+            <strong>Priority:</strong>
+          </label>
           <select
             name="priority"
             value={newIdea.priority}
@@ -97,12 +102,65 @@ const StartupIdeasApp = () => {
             <option value="Medium">Medium</option>
             <option value="High">High</option>
           </select>
-        </label>
-        <br />
+        </div>
         <button type="button" onClick={addIdea}>
           Add Idea
         </button>
       </form>
+      <h2>Ideas</h2>
+      <ul>
+        {sortedIdeas.map((idea) => (
+          <li key={idea.id}>
+            <div className="input-container">
+              <label>
+                <strong>Title:</strong>
+              </label>
+              {editingIdeaId === idea.id ? (
+                <input
+                  type="text"
+                  name="title"
+                  value={newIdea.title}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                idea.title
+              )}
+            </div>
+            <div className="input-container">
+              <label>
+                <strong>Description:</strong>
+              </label>
+              {editingIdeaId === idea.id ? (
+                <input
+                  type="text"
+                  name="description"
+                  value={newIdea.description}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                idea.description
+              )}
+            </div>
+            <div className="input-container">
+              <label>
+                <strong>Priority:</strong>
+              </label>
+              {idea.priority}
+            </div>
+            {editingIdeaId === idea.id ? (
+              <div>
+                <button onClick={() => saveEdit(idea.id)}>Save</button>
+                <button onClick={cancelEdit}>Cancel</button>
+              </div>
+            ) : (
+              <div>
+                <button onClick={() => startEdit(idea.id)}>Edit</button>
+                <button onClick={() => deleteIdea(idea.id)}>Delete</button>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
